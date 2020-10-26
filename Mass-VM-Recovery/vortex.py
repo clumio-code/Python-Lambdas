c6d89f807b7dc2bc6725f36eb04f828fa81f41f0
Luke Mccleary <32186417+lukeb0t@users.noreply.github.com>
2020-10-26 16:34:57 -0400

    Update vortex.py

--- Mass-VM-Recovery/vortex.py
+++ Mass-VM-Recovery/vortex.py
@@ -4,7 +4,7 @@ import requests
 
 
 def get_VM_backups(base_url, version, token, vmid, vcid, latest=False):
-    resource = '/api/backups/vmware/vms'
+    resource = '/backups/vmware/vms'
     filter = {
         "vcenter_id": {"$eq": vcid},
         "vm_id": {"$eq": vmid}
@@ -19,21 +19,21 @@ def get_VM_backups(base_url, version, token, vmid, vcid, latest=False):
 
 
 def get_PIT_params(base_url, version, token, pit_id):
-    url = f'{base_url}/api/backups/vmware/vms/{pit_id}'
+    url = f'{base_url}/backups/vmware/vms/{pit_id}'
     time.sleep(3)
     response = http_get(url, version, token=token)
     return response
 
 
 def recoverVM(base_url, version, token, body):
-    url = f'{base_url}/api/restores/vmware/vms'
+    url = f'{base_url}/restores/vmware/vms'
     response = http_post(url, version, token, body=body)
     return response
 
 
 def list_vm(base_url, version, token, vcid,
             vmname: str, idlookup=False):
-    resource = f'/api/datasources/vmware/vcenters/{vcid}/vms'
+    resource = f'/datasources/vmware/vcenters/{vcid}/vms'
     filter = {
         "name": {"$contains": vmname}
     }
@@ -41,7 +41,9 @@ def list_vm(base_url, version, token, vcid,
         url = f'{base_url}{resource}?limit=500&filter={json.dumps(filter)}'
     else:
         url = f'{base_url}{resource}?limit=500'
+    
     response = http_get(url, version, token=token)
+    
     if idlookup:
         for i in range(len(response['_embedded']['items'])):
             if response['_embedded']['items'][i]['name'] == vmname:
@@ -51,43 +53,43 @@ def list_vm(base_url, version, token, vcid,
 
 
 def list_vc(base_url, version, token):
-    url = f'{base_url}/api/datasources/vmware/vcenters?limit=500'
+    url = f'{base_url}/datasources/vmware/vcenters?limit=500'
     response = http_get(url, version, token=token)
     return response['_embedded']['items']
 
 
 def list_tags(base_url, version, token, vcid):
-    url = f'{base_url}/api/datasources/vmware/vcenters/{vcid}/tags?limit=500'
+    url = f'{base_url}/datasources/vmware/vcenters/{vcid}/tags?limit=500'
     response = http_get(url, version, token=token)
     return response['_embedded']['items']
 
 
 def list_vc_pools(base_url, version, token, vcid):
-    url = f'{base_url}/api/datasources/vmware/vcenters/{vcid}/resource-pools?limit=500'
+    url = f'{base_url}/datasources/vmware/vcenters/{vcid}/resource-pools?limit=500'
     response = http_get(url, version, token=token)
     return response['_embedded']['items']
 
 
 def list_vc_datacenters(base_url, version, token, vcid):
-    url = f'{base_url}/api/datasources/vmware/vcenters/{vcid}/datacenters?limit=500'
+    url = f'{base_url}/datasources/vmware/vcenters/{vcid}/datacenters?limit=500'
     response = http_get(url, version, token=token)
     return response['_embedded']['items']
 
 
 def list_vc_datastores(base_url, version, token, vcid):
-    url = f'{base_url}/api/datasources/vmware/vcenters/{vcid}/datastores?limit=500'
+    url = f'{base_url}/datasources/vmware/vcenters/{vcid}/datastores?limit=500'
     response = http_get(url, version, token=token)
     return response['_embedded']['items']
 
 
 def list_vc_folders(base_url, version, token, vcid):
-    url = f'{base_url}/api/datasources/vmware/vcenters/{vcid}/folders?limit=500'
+    url = f'{base_url}/datasources/vmware/vcenters/{vcid}/folders?limit=500'
     response = http_get(url, version, token=token)
     return response['_embedded']['items']
 
 
 def list_vc_networks(base_url, version, token, vcid):
-    url = f'{base_url}/api/datasources/vmware/vcenters/{vcid}/networks?limit=500'
+    url = f'{base_url}/datasources/vmware/vcenters/{vcid}/networks?limit=500'
     response = http_get(url, version, token=token)
     return response['_embedded']['items']
 
@@ -101,7 +103,8 @@ def http_get(url: str, version: int, token: str):
     headers['Authorization'] = f'Bearer {token}'
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        print(f'HTTP POST to {url} failed with code {response.status_code}')
+        print(f'HTTP GET to {url} failed with code {response.status_code}')
+        print(response.status_code)
         return -1
     return response.json()
 
@@ -112,5 +115,6 @@ def http_post(url: str, version: int, token: str, body=None):
     response = requests.post(url, headers=headers, data=json.dumps(body))
     if response.status_code != 200:
         print(f'HTTP POST to {url} failed with code {response.status_code}')
+        print(response.status_code)
         return -1
     return response.json()
